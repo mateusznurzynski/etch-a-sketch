e8054ab07e4316cfc2f7c3f9e23f6b7d3f9269c4
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-07-01 18:42:12 +0200

    Make the grid resizable with just one variable

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const container = document.querySelector('.container');
 const segment = document.createElement('div');
 segment.classList.add('segment');
 let segmentsNumber = 16;
+segment.style.width = `calc(100%/${segmentsNumber})`;
+segment.style.height = `calc(100%/${segmentsNumber})`;
+segment.style.backgroundColor = 'green';
 
 for (let i = 0; i < segmentsNumber ** 2; i++) {
   container.appendChild(segment.cloneNode(true));
