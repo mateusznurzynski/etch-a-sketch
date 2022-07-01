129d05ba91e40a11135563ae38aa1906e6fb32ff
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-07-01 19:29:28 +0200

    Make the form reset after each submit

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const container = document.querySelector('.container');
 const segment = document.createElement('div');
 const form = document.querySelector('.grid-edit');
-const sizeNumberInput = document.querySelector('#size-number');
 segment.classList.add('segment');
 let segmentsNumber = 16;
 segment.style.width = `calc(100%/${segmentsNumber})`;
@@ -24,7 +23,15 @@ function setGrid(segmentsNumber) {
 }
 
 function submitForm(e) {
+  const sizeNumberInput = document.querySelector('#size-number');
   e.preventDefault();
   console.log(sizeNumberInput.value);
   setGrid(sizeNumberInput.value);
+  clearInputs(sizeNumberInput);
+}
+
+function clearInputs(...inputs) {
+  inputs.forEach((input) => {
+    input.value = '';
+  });
 }
