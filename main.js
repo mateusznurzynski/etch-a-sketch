583e5da41d526467f17ef04e5d5d0965fbc006b8
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-07-01 19:05:18 +0200

    Add a form that will edit the grid

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const container = document.querySelector('.container');
 const segment = document.createElement('div');
+const form = document.querySelector('.grid-edit');
 segment.classList.add('segment');
 let segmentsNumber = 16;
 segment.style.width = `calc(100%/${segmentsNumber})`;
 segment.style.height = `calc(100%/${segmentsNumber})`;
-segment.style.backgroundColor = 'green';
 
 for (let i = 0; i < segmentsNumber ** 2; i++) {
   container.appendChild(segment.cloneNode(true));
